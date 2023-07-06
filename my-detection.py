951c9e10e926f88b96db9335bafff1f475c0f05c
Ashvikraina <ashvikraina@gmail.com>
2023-07-05 18:37:03 -0700

    initial commit

---
+++ my-detection.py
@@ -0,0 +1,34 @@
+""" import jetson.inference
+import jetson.utils
+
+
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = jetson.utils.videoSource("/dev/video0")
+display = jetson.utils.videoOutput()
+
+while True:
+    img=camera.Capture()
+    detections=net.Detect(img)
+    display.Render(img)
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS())) """
+
+from jetson_inference import detectNet
+import jetson.utils
+from jetson_utils import videoSource, videoOutput
+
+net = detectNet("ssd-mobilenet-v2", threshold=0.5)
+camera = videoSource('/dev/video0')      # '/dev/video0' for V4L2
+display = jetson.utils.videoOutput()
+
+while display.IsStreaming():
+    img = camera.Capture()
+
+    if img is None: # capture timeout
+        continue
+
+    detections = net.Detect(img)
+    
+    print("sup")
+    display.Render(img)
+    print('hey')
+    display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))

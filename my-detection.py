6a540eaa152345965f05778c5e3eafbaab7f1f67
Ashvikraina <ashvikraina@gmail.com>
2023-07-08 13:51:53 -0700

    initial commit

--- my-detection.py
+++ my-detection.py
@@ -16,20 +16,28 @@ import jetson.inference
 from jetson.inference import detectNet
 import jetson.utils
 from jetson.utils import videoSource, videoOutput
+import time
+# from playsound import playsound
 
 net = detectNet("ssd-mobilenet-v2", threshold=0.5)
-camera = videoSource('/dev/video0')      # '/dev/video0' for V4L2
-display = jetson.utils.videoOutput()
-
-while display.IsStreaming():
+camera = videoSource('/dev/video0')
+display = jetson.utils.videoOutput("detecttest1.mp4")
+# playsound("alarm-clock-short-6402.mp3")
+while True:
     img = camera.Capture()
 
-    if img is None: # capture timeout
+    if img is None: 
         continue
 
+    current_time=time.strftime("%Y-%m-%d %H:%M:%S")
+
     detections = net.Detect(img)
+
+    for detection in detections:
+        confidence=detection.Confidence
+        class_id=detection.ClassID
+        class_name=net.GetClassDesc(class_id)
+        print(f"At {current_time} a {class_name} was detected with {confidence:.2f} confidence.")
     
     display.Render(img)
     display.SetStatus("Object Detection | Network {:.0f} FPS".format(net.GetNetworkFPS()))
-
-    #test
